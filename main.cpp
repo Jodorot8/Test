57ecc5febe4762d9faa25b6c1b24485843e610c5
Jodorot8 <markushossner@gmx.de>
2024-08-20 22:06:34 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,20 @@
 #include <iostream>
 #include <cstring>
 
-using namespace std;
-
 void vulnerableFunction(char *input) {
     char buffer[10];
-    // Pufferüberlauf: Die Eingabe wird direkt in einen kleinen Puffer kopiert
     strcpy(buffer, input);
-    cout << "Buffer Inhalt: " << buffer << endl;
+    std::cout << "Buffer Inhalt: " << buffer << std::endl;
 }
 
 int main() {
     char *dynamicArray = new char[10];
     
-    // Unsichere Verwendung von new: Der Speicher wird nicht überprüft, ob er erfolgreich zugewiesen wurde
-    cout << "Geben Sie einen Text ein: ";
-    cin >> dynamicArray;  // Pufferüberlauf: Es wird keine Länge des Eingabe-Strings überprüft
+    std::cout << "Geben Sie einen Text ein: ";
+    std::cin >> dynamicArray;  // Pufferüberlauf: Es wird keine Länge des Eingabe-Strings überprüft
     
     vulnerableFunction(dynamicArray);
     
-    // Speicherleck: Der Speicher wird nicht freigegeben
+    delete[] dynamicArray;  // Speicher freigeben, um Speicherleck zu vermeiden
     return 0;
 }
